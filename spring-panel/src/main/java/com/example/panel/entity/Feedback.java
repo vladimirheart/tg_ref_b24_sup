6abec6508bf465fbf8785b9427b71c52197521fa
Vladimir_SV <autoschool34@gmail.com>
2025-10-31 13:17:33 +0300

    Create Feedback.java

---
+++ spring-panel/src/main/java/com/example/panel/entity/Feedback.java
@@ -0,0 +1,28 @@
+package com.example.panel.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "feedbacks")
+@Getter
+@Setter
+public class Feedback {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Integer rating;
+
+    private OffsetDateTime timestamp;
+}
