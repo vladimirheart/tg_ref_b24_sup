17eb40665275c0fa48673fa277311e230f81dbf5
Vladimir_SV <autoschool34@gmail.com>
2025-10-31 14:05:27 +0300

    Create ItEquipmentCatalogRepository.java

---
+++ spring-panel/src/main/java/com/example/panel/repository/ItEquipmentCatalogRepository.java
@@ -0,0 +1,7 @@
+package com.example.panel.repository;
+
+import com.example.panel.entity.ItEquipmentCatalog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItEquipmentCatalogRepository extends JpaRepository<ItEquipmentCatalog, Long> {
+}
