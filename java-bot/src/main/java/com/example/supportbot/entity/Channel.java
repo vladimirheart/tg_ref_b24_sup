0899e356ee70c5e999e8ca692b01dfd178abcf7d
Vladimir_SV <autoschool34@gmail.com>
2025-10-31 11:16:22 +0300

    Create Channel.java

---
+++ java-bot/src/main/java/com/example/supportbot/entity/Channel.java
@@ -0,0 +1,42 @@
+package com.example.supportbot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "channels")
+public class Channel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token", length = 128)
+    private String token;
+
+    @Column(name = "public_id", length = 64)
+    private String publicId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
+}
