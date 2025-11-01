2725b646e2a5b928eadb3d790876ccdfae1b8da5
Vladimir_SV <autoschool34@gmail.com>
2025-11-01 10:11:52 +0300

    Update NotificationRepository.java

--- spring-panel/src/main/java/com/example/panel/repository/NotificationRepository.java
+++ spring-panel/src/main/java/com/example/panel/repository/NotificationRepository.java
@@ -4,8 +4,13 @@ import com.example.panel.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findByUserIdentityOrderByCreatedAtDesc(String userIdentity);
+
+    long countByUserIdentityAndIsReadFalse(String userIdentity);
+
+    Optional<Notification> findByIdAndUserIdentity(Long id, String userIdentity);
 }
