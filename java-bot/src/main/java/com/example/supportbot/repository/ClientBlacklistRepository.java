46f717e6a02c4bb35b752d3a54a27594e2aeaffc
Vladimir_SV <autoschool34@gmail.com>
2025-11-06 08:45:51 +0300

    Delete java-bot directory

--- java-bot/src/main/java/com/example/supportbot/repository/ClientBlacklistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.supportbot.repository;
-
-import com.example.supportbot.entity.ClientBlacklist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientBlacklistRepository extends JpaRepository<ClientBlacklist, String> {
-}
