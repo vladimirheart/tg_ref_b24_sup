303c160fd074ec343b3642b32a8b3d718de42ec8
Vladimir_SV <autoschool34@gmail.com>
2025-11-24 17:08:03 +0300

    Update TaskRepository.java

--- spring-panel/src/main/java/com/example/panel/repository/TaskRepository.java
+++ spring-panel/src/main/java/com/example/panel/repository/TaskRepository.java
@@ -3,5 +3,9 @@ package com.example.panel.repository;
 import com.example.panel.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findTop50ByOrderByCreatedAtDesc();
 }
