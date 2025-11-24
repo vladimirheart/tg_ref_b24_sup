c5d16f82524048db485460707c9f07db19add4b1
Vladimir_SV <autoschool34@gmail.com>
2025-11-24 15:31:25 +0300

    Create TicketRepository.java

---
+++ java-bot/src/main/java/com/example/supportbot/repository/TicketRepository.java
@@ -0,0 +1,10 @@
+package com.example.supportbot.repository;
+
+import com.example.supportbot.entity.Ticket;
+import com.example.supportbot.entity.TicketId;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketRepository extends JpaRepository<Ticket, TicketId> {
+    Optional<Ticket> findTopByIdUserIdOrderByIdTicketIdDesc(Long userId);
+}
