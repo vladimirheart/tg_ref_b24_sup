e8cc25f719ffbdba974482501cdd523b75fd405c
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 17:35:02 +0300

    Update TaskLink.java

--- spring-panel/src/main/java/com/example/panel/entity/TaskLink.java
+++ spring-panel/src/main/java/com/example/panel/entity/TaskLink.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinColumns;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
