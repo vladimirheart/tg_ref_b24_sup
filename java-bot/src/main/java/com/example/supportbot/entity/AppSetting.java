d997c7042373e522dd17757305df0c49e0a0af2c
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 09:02:49 +0300

    Create AppSetting.java

---
+++ java-bot/src/main/java/com/example/supportbot/entity/AppSetting.java
@@ -0,0 +1,54 @@
+package com.example.supportbot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "app_settings")
+public class AppSetting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "channel_id")
+    private Channel channel;
+
+    @Column(name = "key")
+    private String key;
+
+    @Column(name = "value", columnDefinition = "TEXT")
+    private String value;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
