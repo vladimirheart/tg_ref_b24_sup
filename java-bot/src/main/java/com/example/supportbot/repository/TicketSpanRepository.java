eabafe78d03d647bc5c36014eb2a8fddf0d8e978
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 09:56:09 +0300

    Update TicketSpanRepository.java

--- java-bot/src/main/java/com/example/supportbot/repository/TicketSpanRepository.java
+++ java-bot/src/main/java/com/example/supportbot/repository/TicketSpanRepository.java
@@ -1,7 +1,12 @@
 package com.example.supportbot.repository;
 
 import com.example.supportbot.entity.TicketSpan;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketSpanRepository extends JpaRepository<TicketSpan, Long> {
+
+    Optional<TicketSpan> findTopByTicketIdOrderBySpanNumberDesc(String ticketId);
+
+    Optional<TicketSpan> findFirstByTicketIdAndEndedAtIsNullOrderBySpanNumberDesc(String ticketId);
 }
