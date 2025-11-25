a673190f058e0683b27132ad153c2463443aa7a8
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 09:23:38 +0300

    Create ItEquipmentItemRepository.java

---
+++ java-bot/src/main/java/com/example/supportbot/repository/ItEquipmentItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.supportbot.repository;
+
+import com.example.supportbot.entity.ItEquipmentItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItEquipmentItemRepository extends JpaRepository<ItEquipmentItem, Long> {
+}
