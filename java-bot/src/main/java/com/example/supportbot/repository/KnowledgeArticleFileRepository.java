a9d0f7d28c7760a174ffe44663d13a465a26a1ab
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 13:51:13 +0300

    Update KnowledgeArticleFileRepository.java

--- java-bot/src/main/java/com/example/supportbot/repository/KnowledgeArticleFileRepository.java
+++ java-bot/src/main/java/com/example/supportbot/repository/KnowledgeArticleFileRepository.java
@@ -1,7 +1,12 @@
 package com.example.supportbot.repository;
 
 import com.example.supportbot.entity.KnowledgeArticleFile;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface KnowledgeArticleFileRepository extends JpaRepository<KnowledgeArticleFile, Long> {
+
+    List<KnowledgeArticleFile> findByDraftToken(String draftToken);
+
+    List<KnowledgeArticleFile> findByArticleId(Long articleId);
 }
