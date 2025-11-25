f73802d37a370981a07ecb15ecdb284020650e30
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 17:18:31 +0300

    Update TaskLinkId.java

--- spring-panel/src/main/java/com/example/panel/entity/TaskLinkId.java
+++ spring-panel/src/main/java/com/example/panel/entity/TaskLinkId.java
@@ -16,6 +16,9 @@ public class TaskLinkId implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Column(name = "user_id")
+    private Long userId;
+
     @Column(name = "task_id")
     private Long taskId;
 
