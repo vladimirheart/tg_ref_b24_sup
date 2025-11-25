0f76d198a6e3ecbaae6db1c9407e297ed28d3530
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 09:30:00 +0300

    Create TicketSpanRepository.java

---
+++ java-bot/src/main/java/com/example/supportbot/repository/TicketSpanRepository.java
@@ -0,0 +1,7 @@
+package com.example.supportbot.repository;
+
+import com.example.supportbot.entity.TicketSpan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketSpanRepository extends JpaRepository<TicketSpan, Long> {
+}
