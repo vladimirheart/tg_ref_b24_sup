19391fe78f7f0eeacda31223bd50e7759bdb2e42
Vladimir_SV <autoschool34@gmail.com>
2025-11-25 09:12:52 +0300

    Create TaskLink.java

---
+++ java-bot/src/main/java/com/example/supportbot/entity/TaskLink.java
@@ -0,0 +1,42 @@
+package com.example.supportbot.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "task_links")
+public class TaskLink {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "task_id")
+    private Long taskId;
+
+    @Column(name = "ticket_id")
+    private String ticketId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Long taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+}
