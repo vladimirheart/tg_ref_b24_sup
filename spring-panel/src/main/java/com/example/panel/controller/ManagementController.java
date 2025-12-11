8abc67fa6cfcb840ec45ffc4ac88f53f54810df1
Vladimir_SV <autoschool34@gmail.com>
2025-12-11 14:11:12 +0300

    Update ManagementController.java

--- spring-panel/src/main/java/com/example/panel/controller/ManagementController.java
+++ spring-panel/src/main/java/com/example/panel/controller/ManagementController.java
@@ -56,6 +56,7 @@ public class ManagementController {
         List<Task> tasks = taskRepository.findTop50ByOrderByCreatedAtDesc();
         model.addAttribute("tasks", tasks);
         return "tasks/index";
+        model.addAttribute("users", panelUserRepository.findAll());
     }
 
     @GetMapping("/channels")
