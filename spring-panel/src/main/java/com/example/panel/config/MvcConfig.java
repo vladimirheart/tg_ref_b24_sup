5318351bfc6e92ad796212386cae46986519a7dd
Vladimir_SV <autoschool34@gmail.com>
2025-12-16 17:41:57 +0300

    Update MvcConfig.java

--- spring-panel/src/main/java/com/example/panel/config/MvcConfig.java
+++ spring-panel/src/main/java/com/example/panel/config/MvcConfig.java
@@ -10,5 +10,9 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("auth/login");
+
+        registry.addViewController("/error/403").setViewName("error/403");
+        registry.addViewController("/error/404").setViewName("error/404");
+        registry.addViewController("/error/500").setViewName("error/500");
     }
 }
