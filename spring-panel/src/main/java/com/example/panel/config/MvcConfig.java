f0da4ab0c3df8a1e3b9b2b69cbda76ef23afddbe
Vladimir_SV <autoschool34@gmail.com>
2025-12-30 12:55:48 +0300

    Update MvcConfig.java

--- spring-panel/src/main/java/com/example/panel/config/MvcConfig.java
+++ spring-panel/src/main/java/com/example/panel/config/MvcConfig.java
@@ -1,11 +1,17 @@
 package com.example.panel.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
