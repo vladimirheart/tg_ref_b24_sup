583dc37d1cf7c8a31aeb3472aa5b178b024a0232
Vladimir_SV <autoschool34@gmail.com>
2026-01-13 17:30:04 +0300

    Delete panel/secret_utils.py

--- panel/secret_utils.py
+++
@@ -1,9 +0,0 @@
-"""Backwards compatible import layer for secret helpers."""
-from core.secrets import (  # noqa: F401
-    SecretStorageError,
-    decrypt_token,
-    encrypt_token,
-    mask_token,
-)
-
-__all__ = ["encrypt_token", "decrypt_token", "mask_token", "SecretStorageError"]
