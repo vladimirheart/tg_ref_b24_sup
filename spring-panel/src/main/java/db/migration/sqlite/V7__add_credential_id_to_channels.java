3a1fce5afb248ea889ee1d868bd06cc75f790e6c
Vladimir_SV <autoschool34@gmail.com>
2026-01-14 12:24:33 +0300

    Create V7__add_credential_id_to_channels.java

---
+++ spring-panel/src/main/java/db/migration/sqlite/V7__add_credential_id_to_channels.java
@@ -0,0 +1,33 @@
+package db.migration.sqlite;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+
+public class V7__add_credential_id_to_channels extends BaseJavaMigration {
+
+    @Override
+    public void migrate(Context context) throws Exception {
+        if (columnExists(context.getConnection(), "channels", "credential_id")) {
+            return;
+        }
+
+        try (Statement statement = context.getConnection().createStatement()) {
+            statement.execute("ALTER TABLE channels ADD COLUMN credential_id BIGINT");
+        }
+    }
+
+    private boolean columnExists(Connection connection, String tableName, String columnName) throws SQLException {
+        String sql = String.format("SELECT 1 FROM pragma_table_info('%s') WHERE name = ?", tableName);
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, columnName);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next();
+            }
+        }
+    }
+}
