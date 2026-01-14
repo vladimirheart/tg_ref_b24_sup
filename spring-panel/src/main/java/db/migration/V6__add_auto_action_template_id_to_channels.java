37a1b417baa76440aef826641a167e6062b83725
Vladimir_SV <autoschool34@gmail.com>
2026-01-14 11:48:46 +0300

    Update V6__add_auto_action_template_id_to_channels.java

--- spring-panel/src/main/java/db/migration/V6__add_auto_action_template_id_to_channels.java
+++ spring-panel/src/main/java/db/migration/V6__add_auto_action_template_id_to_channels.java
@@ -9,30 +9,25 @@ import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
 public class V6__add_auto_action_template_id_to_channels extends BaseJavaMigration {
-    @Override
-    public void migrate(Context context) throws SQLException {
-        try (Connection connection = context.getConnection()) {
-            if (columnExists(connection)) {
-                return;
-            }
 
-            try (Statement statement = connection.createStatement()) {
-                statement.execute("ALTER TABLE channels ADD COLUMN auto_action_template_id TEXT");
-            }
+    @Override
+    public void migrate(Context context) throws Exception {
+        if (columnExists(context.getConnection(), "channels", "auto_action_template_id")) {
+            return;
+        }
+
+        try (Statement statement = context.getConnection().createStatement()) {
+            statement.execute("ALTER TABLE channels ADD COLUMN auto_action_template_id TEXT");
         }
     }
 
-    private boolean columnExists(Connection connection) throws SQLException {
-        String sql = "PRAGMA table_info('channels')";
-        try (PreparedStatement statement = connection.prepareStatement(sql);
-             ResultSet resultSet = statement.executeQuery()) {
-            while (resultSet.next()) {
-                String name = resultSet.getString("name");
-                if ("auto_action_template_id".equalsIgnoreCase(name)) {
-                    return true;
-                }
+    private boolean columnExists(Connection connection, String tableName, String columnName) throws SQLException {
+        String sql = String.format("SELECT 1 FROM pragma_table_info('%s') WHERE name = ?", tableName);
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, columnName);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next();
             }
         }
-        return false;
     }
 }
