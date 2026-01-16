be09f91412f68854102ff1e6a31ec835731c9bab
Vladimir_SV <autoschool34@gmail.com>
2026-01-16 15:07:58 +0300

    Add files via upload

---
+++ java-bot/bot-core/src/main/java/com/example/supportbot/repository/TicketMessageRepository.java
@@ -0,0 +1,9 @@
+package com.example.supportbot.repository;
+
+import com.example.supportbot.entity.TicketMessage;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TicketMessageRepository extends JpaRepository<TicketMessage, Long> {
+    Optional<TicketMessage> findTopByUserIdOrderByCreatedAtDesc(Long userId);
+}
