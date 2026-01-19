ab70cb1170221c07c36e95e538e855cc283b9fdc
Vladimir_SV <autoschool34@gmail.com>
2026-01-19 17:40:48 +0300

    Update TelegramBotApplication.java

--- java-bot/bot-telegram/src/main/java/com/example/supportbot/telegram/TelegramBotApplication.java
+++ java-bot/bot-telegram/src/main/java/com/example/supportbot/telegram/TelegramBotApplication.java
@@ -2,9 +2,13 @@ package com.example.supportbot.telegram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.example.supportbot")
+@EnableJpaRepositories(basePackages = "com.example.supportbot.repository")
+@EntityScan(basePackages = "com.example.supportbot")
 @EnableScheduling
 public class TelegramBotApplication {
 
