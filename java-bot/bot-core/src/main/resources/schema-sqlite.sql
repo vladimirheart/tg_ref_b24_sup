57124299aa8fbed924840802c37fad5a5a854966
Vladimir_SV <autoschool34@gmail.com>
2026-01-30 18:20:40 +0300

    Update schema-sqlite.sql

--- java-bot/bot-core/src/main/resources/schema-sqlite.sql
+++ java-bot/bot-core/src/main/resources/schema-sqlite.sql
@@ -275,13 +275,13 @@ CREATE TABLE IF NOT EXISTS ticket_responsibles (
 
 CREATE TABLE IF NOT EXISTS ticket_active (
     ticket_id TEXT PRIMARY KEY,
-    user TEXT NOT NULL,
+    user_identity TEXT NOT NULL,
     last_seen TEXT DEFAULT (datetime('now'))
 );
 
 CREATE TABLE IF NOT EXISTS notifications (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user TEXT NOT NULL,
+    user_identity TEXT NOT NULL,
     text TEXT NOT NULL,
     url TEXT,
     is_read INTEGER DEFAULT 0,
