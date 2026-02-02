3d6adfa6f702c3b7b0f9ed8e93ed2c1a96c0d2b9
Vladimir_SV <autoschool34@gmail.com>
2026-02-02 17:58:41 +0300

    Update ClientBlacklist.java

--- spring-panel/src/main/java/com/example/panel/entity/ClientBlacklist.java
+++ spring-panel/src/main/java/com/example/panel/entity/ClientBlacklist.java
@@ -4,15 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "client_blacklist")
-@Getter
-@Setter
 public class ClientBlacklist {
 
     @Id
@@ -31,4 +27,60 @@ public class ClientBlacklist {
     private Boolean unblockRequested;
 
     private OffsetDateTime unblockRequestedAt;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Boolean getBlacklisted() {
+        return blacklisted;
+    }
+
+    public void setBlacklisted(Boolean blacklisted) {
+        this.blacklisted = blacklisted;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public OffsetDateTime getAddedAt() {
+        return addedAt;
+    }
+
+    public void setAddedAt(OffsetDateTime addedAt) {
+        this.addedAt = addedAt;
+    }
+
+    public String getAddedBy() {
+        return addedBy;
+    }
+
+    public void setAddedBy(String addedBy) {
+        this.addedBy = addedBy;
+    }
+
+    public Boolean getUnblockRequested() {
+        return unblockRequested;
+    }
+
+    public void setUnblockRequested(Boolean unblockRequested) {
+        this.unblockRequested = unblockRequested;
+    }
+
+    public OffsetDateTime getUnblockRequestedAt() {
+        return unblockRequestedAt;
+    }
+
+    public void setUnblockRequestedAt(OffsetDateTime unblockRequestedAt) {
+        this.unblockRequestedAt = unblockRequestedAt;
+    }
 }
