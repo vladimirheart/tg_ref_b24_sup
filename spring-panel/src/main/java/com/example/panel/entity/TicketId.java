2ec052e4db4ea1a520cf1a41c39c3cb5ee5a7d66
Vladimir_SV <autoschool34@gmail.com>
2026-02-02 18:05:36 +0300

    Update TicketId.java

--- spring-panel/src/main/java/com/example/panel/entity/TicketId.java
+++ spring-panel/src/main/java/com/example/panel/entity/TicketId.java
@@ -2,16 +2,10 @@ package com.example.panel.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.io.Serializable;
 
 @Embeddable
-@Getter
-@Setter
-@EqualsAndHashCode
 public class TicketId implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -21,4 +15,42 @@ public class TicketId implements Serializable {
 
     @Column(name = "ticket_id")
     private String ticketId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TicketId ticketId1 = (TicketId) o;
+        if (userId != null ? !userId.equals(ticketId1.userId) : ticketId1.userId != null) {
+            return false;
+        }
+        return ticketId != null ? ticketId.equals(ticketId1.ticketId) : ticketId1.ticketId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (ticketId != null ? ticketId.hashCode() : 0);
+        return result;
+    }
 }
