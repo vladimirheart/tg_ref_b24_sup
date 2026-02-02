3af84a70b7be1d755bd940f652a087ef76c48a5c
Vladimir_SV <autoschool34@gmail.com>
2026-02-02 17:42:12 +0300

    Update ClientUsername.java

--- spring-panel/src/main/java/com/example/panel/entity/ClientUsername.java
+++ spring-panel/src/main/java/com/example/panel/entity/ClientUsername.java
@@ -28,4 +28,36 @@ public class ClientUsername {
 
     @Convert(converter = LenientOffsetDateTimeConverter.class)
     private OffsetDateTime seenAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public OffsetDateTime getSeenAt() {
+        return seenAt;
+    }
+
+    public void setSeenAt(OffsetDateTime seenAt) {
+        this.seenAt = seenAt;
+    }
 }
