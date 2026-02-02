032a117326df728c70178f72383a938a551cbd54
Vladimir_SV <autoschool34@gmail.com>
2026-02-02 11:47:22 +0300

    Update TicketActiveRepository.java

--- java-bot/bot-core/src/main/java/com/example/supportbot/repository/TicketActiveRepository.java
+++ java-bot/bot-core/src/main/java/com/example/supportbot/repository/TicketActiveRepository.java
@@ -1,7 +1,9 @@
 package com.example.supportbot.repository;
 
 import com.example.supportbot.entity.TicketActive;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketActiveRepository extends JpaRepository<TicketActive, String> {
+    List<TicketActive> findByUserInOrderByLastSeenDesc(List<String> users);
 }
