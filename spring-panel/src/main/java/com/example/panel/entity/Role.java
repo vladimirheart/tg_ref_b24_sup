5b0f510eb4518e8455b74ede7dbabb618057bf4a
Vladimir_SV <autoschool34@gmail.com>
2026-02-02 17:44:56 +0300

    Update Role.java

--- spring-panel/src/main/java/com/example/panel/entity/Role.java
+++ spring-panel/src/main/java/com/example/panel/entity/Role.java
@@ -25,4 +25,36 @@ public class Role {
 
     @Column(columnDefinition = "jsonb")
     private String permissions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
 }
