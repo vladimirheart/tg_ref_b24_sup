93f3c72ecdf4c59adef0114ba82aee9faa06b53c
Vladimir_SV <autoschool34@gmail.com>
2026-02-03 14:40:02 +0300

    Update FeedbackRepository.java

--- java-bot/bot-core/src/main/java/com/example/supportbot/repository/FeedbackRepository.java
+++ java-bot/bot-core/src/main/java/com/example/supportbot/repository/FeedbackRepository.java
@@ -1,7 +1,9 @@
 package com.example.supportbot.repository;
 
 import com.example.supportbot.entity.Feedback;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+    List<Feedback> findByTicketIdIn(List<String> ticketIds);
 }
