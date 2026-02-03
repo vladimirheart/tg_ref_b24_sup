6d4ff696baaefe313253a859c592dd86aba09876
Vladimir_SV <autoschool34@gmail.com>
2026-02-03 11:50:21 +0300

    Update ClientProfileHeader.java

--- spring-panel/src/main/java/com/example/panel/model/clients/ClientProfileHeader.java
+++ spring-panel/src/main/java/com/example/panel/model/clients/ClientProfileHeader.java
@@ -3,6 +3,9 @@ package com.example.panel.model.clients;
 public record ClientProfileHeader(
         Long userId,
         String username,
-        String clientName
+        String clientName,
+        Long channelId,
+        String channelName,
+        String platform
 ) {
 }
