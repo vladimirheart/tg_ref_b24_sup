f40b83044855407f9db5d1d79cb99688b0fc3342
Vladimir_SV <autoschool34@gmail.com>
2026-02-05 15:39:56 +0300

    Update and rename V7__add_credential_id_to_channels.java to V7_1__add_credential_id_to_channels.java

--- spring-panel/src/main/java/db/migration/sqlite/V7_1__add_credential_id_to_channels.java
+++ spring-panel/src/main/java/db/migration/sqlite/V7_1__add_credential_id_to_channels.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-public class V7__add_credential_id_to_channels extends BaseJavaMigration {
+public class V7_1__add_credential_id_to_channels extends BaseJavaMigration {
 
     @Override
     public void migrate(Context context) throws Exception {
