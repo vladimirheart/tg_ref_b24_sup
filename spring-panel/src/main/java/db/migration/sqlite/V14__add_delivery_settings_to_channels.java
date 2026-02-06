43cb28819d61c697398d8eea9e0806bd6c5ce91d
Vladimir_SV <autoschool34@gmail.com>
2026-02-06 11:26:52 +0300

    Update and rename V9__add_delivery_settings_to_channels.java to V14__add_delivery_settings_to_channels.java

--- spring-panel/src/main/java/db/migration/sqlite/V14__add_delivery_settings_to_channels.java
+++ spring-panel/src/main/java/db/migration/sqlite/V14__add_delivery_settings_to_channels.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-public class V9__add_delivery_settings_to_channels extends BaseJavaMigration {
+public class V14__add_delivery_settings_to_channels extends BaseJavaMigration {
 
     @Override
     public void migrate(Context context) throws Exception {
